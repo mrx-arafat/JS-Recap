d5aeb948e9529857f7b7b3e1b1fc53c7c481c198
Easin Arafat <arafatmrx@gmail.com>
2022-03-06 17:12:39 +0600

    num

---
+++ 1. JS fundamental/number.js
@@ -0,0 +1,12 @@
+const sexAge = "69arafat";
+const sexAge2 = parseInt(sexAge);
+
+console.log(sexAge);
+
+console.log(typeof sexAge2);
+
+// tofixed
+
+var x = 191.35832423;
+console.log(x.toFixed(4));
+console.log(x.toPrecision(5));
