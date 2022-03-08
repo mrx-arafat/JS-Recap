9835710e6c18005c93b388085ff02672d144b9d0
Easin Arafat <arafatmrx@gmail.com>
2022-03-08 10:21:09 +0600

    for loop

--- 2. array/array.js
+++ 2. array/array.js
@@ -1,3 +1,13 @@
 var cars = ["bmw", "toyota", "seat", "ferrari"];
 
-console.log(cars[0]);
+console.log(cars);
+console.log(cars.length);
+
+cars[4] = "tesla";
+
+console.log(cars);
+console.log(cars.length);
+
+//print the last element : (length-1) th element
+
+console.log(cars[cars.length - 1]);
---
+++ 2. array/loop.js
@@ -0,0 +1,5 @@
+const cars = ["bmw", "toyota", "seat", "ferrari", "tesla"];
+
+for (i = 0; i <= cars.length - 1; i++) {
+  console.log(i, cars[i]);
+}
