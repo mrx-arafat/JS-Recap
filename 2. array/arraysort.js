4043aaa4c24dbf47526fa8f5afa786b30981d9e3
Easin Arafat <arafatmrx@gmail.com>
2022-03-10 09:53:06 +0600

    num sort

--- 2. array/arraysort.js
+++ 2. array/arraysort.js
@@ -3,3 +3,17 @@ fruits.sort();
 console.log(fruits);
 fruits.reverse();
 console.log(fruits);
+
+/// numeric sorting
+
+const num = [21, 43, 12, 22, 98, 65, 76];
+num.sort(function (a, b) {
+  return b - a;
+});
+//boro theke choto
+console.log(num);
+num.sort(function (a, b) {
+  return a - b;
+});
+//choto theke boro
+console.log(num);
