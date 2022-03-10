244a14a1ce5241d264834dda83e37f0c7d6be3e4
Easin Arafat <arafatmrx@gmail.com>
2022-03-10 12:53:52 +0600

    map

---
+++ 2. array/arraymap.js
@@ -0,0 +1,15 @@
+const nums = [112, 32, 33, 3, 7];
+
+function myFunction(value) {
+  return value * 2;
+}
+
+const newNums = nums.map(myFunction);
+
+console.log(newNums);
+
+//what is the difference between forEach and map
+
+// think once map amare ekta new array return korteche
+
+// forEach amare same array tei change kore dibe
