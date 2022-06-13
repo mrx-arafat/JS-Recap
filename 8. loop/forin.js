31a1c42f8f5a29214aa0b3029c603dec2eaef0a4
mrx-arafat <arafatmrx@gmail.com>
2022-06-13 13:58:36 +0600

    for in

--- 8. loop/forin.js
+++ 8. loop/forin.js
@@ -1,9 +1,7 @@
 const cars = { name: "BMW", model: "z4", price: "120k" };
 // object er moddhe loop calate hole for in
 for (let x in cars) {
-  console.log(x + ":");
-
-  console.log(cars[x]);
+  console.log(`the key is ${x} and it's value is ${cars[x]}`);
 }
 
 //don't use for in over an array>> for of and forEach use kora lagbe
